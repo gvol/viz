2820ec99cba09e56cdcdf13f499e592b8cf1d916
Ivan Andrus <darthandrus@gmail.com>
2012-04-23 11:28:32 +0200

    Add dir-locals.el to help enforce coding standards (in Emacs)

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((indent-tabs-mode . nil)
+         (tab-width . 200) ;; Make them stand out
+         (fill-column . 80)))
+ (gap-mode . ((gap-indent-step . 2)
+              (gap-local-statement-format . (1 1))))
+ )
